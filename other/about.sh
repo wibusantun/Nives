946ec6e925ce3e0517f498a57b31b200c77f2296
Wibu <100986373+wibusantun@users.noreply.github.com>
2025-07-11 17:11:58 +0700

    about.sh

--- other/about.sh
+++ other/about.sh
@@ -11,7 +11,7 @@ clear
 echo ""
 echo ""
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
-echo -e "                     ${WB}PREMIUM VVIP${NC}"
+echo -e "                     ${WB}PREMIUM VVIP${NC}                        "
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
 echo -e "  ${WB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
 echo -e "${BB}—————————————————————————————————————————————————————————${NC}"
